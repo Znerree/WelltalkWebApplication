98682e973a217fbd31f037e2b8659e20d66f5dc8
Jake Errenz Batulan <114793116+Znerree@users.noreply.github.com>
2023-11-18 03:12:13 +0800

    Delete welltalk_backend directory

--- welltalk_backend/src/main/java/com/website/welltalk/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.website.welltalk.repositories;
-
-import com.website.welltalk.models.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, Object> {
-}
