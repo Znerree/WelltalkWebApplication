d49bd4c6a6c20d0f3ac442b0941b9fe9707749e6
Jake Errenz Batulan <114793116+Znerree@users.noreply.github.com>
2023-11-18 04:03:53 +0800

    test2

--- welltalkwebapp_frontend/src/components/landingpage-contents/LandingPageContent.tsx
+++ welltalkwebapp_frontend/src/components/landingpage-contents/LandingPageContent.tsx
@@ -3,7 +3,7 @@ import LandingContentText from "./landingcontent-text";
 
 const LandingPageContent = () => {
   return (
-    <div className=" h-full flex w-[600px] items-center mx-10 gap-5">
+    <div className=" h-full flex md:w-[600px] items-center mx-10 gap-5">
       <span>
         <LandingContentText />
         <LandingContentBottom />
